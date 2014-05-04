69a6bd8ff6282c6741fc4b84afae3341fd81473f
Nikolai Kristiansen <nikolaik@gmail.com>
2014-05-04 20:24:30 +0200

    Minor

--- wp-content/themes/tn-2014/program-upcoming.php
+++ wp-content/themes/tn-2014/program-upcoming.php
@@ -1,5 +1,5 @@
 <div class="program-upcoming">
     <h3>Neste arrangementer</h3>
     <ul class="program-list"><!-- Generated --></ul>
-    <a href="https://studentersamfundet.no/program/?category=teater" class="program-more-link">Vis alle våre arrangementer</a>
+    <a href="https://studentersamfundet.no/program/?event_type=teater" class="program-more-link">Vis alle våre arrangementer</a>
 </div>
