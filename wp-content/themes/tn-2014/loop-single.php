4f7aeefe0169650f4fdca6ea10e74cd72670555e
Jonas Braathen <jonas@ponas.no>
2014-09-08 18:51:15 +0200

    Nah, everything was fine. Let's do the same for loop-single.

--- wp-content/themes/tn-2014/loop-single.php
+++ wp-content/themes/tn-2014/loop-single.php
@@ -5,7 +5,7 @@
 
 				<div class="body">
 
-                    <div class="entry-meta byline"><span class="meta-prep meta-prep-author">av </span><span class="author vcard"><?php the_author_link(); ?></span>, <span class="entry-date"><?php echo date_i18n( get_option('date_format')).", ".date_i18n( get_option('time_format')); ?></span></div>
+                    <div class="entry-meta byline"><span class="meta-prep meta-prep-author">av </span><span class="author vcard"><?php the_author_link(); ?></span>, <span class="entry-date"><?php echo date_i18n( get_option('date_format'), get_post_time('U', true) ).", ".date_i18n( get_option('time_format'), get_post_time('U', true) ); ?></span></div>
 					<?php the_post_thumbnail( 'single' ); ?>
 					<div class="entry-content"><?php the_content(); ?></div> <!-- .entry-content -->
 
